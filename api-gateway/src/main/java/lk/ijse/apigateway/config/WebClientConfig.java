6cb2e283f67e840dcafe7cdaf26d92cb086be31b
nisalsahansith <nisalsahansith@gmail.com>
2026-04-09 18:22:33 +0530

    Add ReadMe File and Screen shots

--- api-gateway/src/main/java/lk/ijse/apigateway/config/WebClientConfig.java
+++ api-gateway/src/main/java/lk/ijse/apigateway/config/WebClientConfig.java
@@ -1,11 +1,18 @@
 package lk.ijse.apigateway.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    @Bean
+    @LoadBalanced // This is crucial for Service Discovery
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 
     @Bean
     public WebClient webClient() {
